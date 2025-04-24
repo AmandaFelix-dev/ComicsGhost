552ef9efe9eba51f1a52723e116c8256d6e1bd0e
Josue Araujo Rodrigues Lima <56053426@sempreuni7.com.br>
2025-04-24 19:58:07 -0300

    fix: alteração do title, 'Home'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>ComicsGhost - Página Inicial</title>
+  <title>ComicsGhost</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="{{ asset('css/home.css') }}" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Wix+Madefor+Display:wght@400..800&display=swap" rel="stylesheet">
